a54843a6ad5f1c2c4a95fc61825542a138dbf26c
Joaquin <jaltable@fi.uba.ar>
2023-11-08 18:56:49 -0300

    changed the 'ProductEdit' content

--- src/Components/ProductEdit/ProductEdit.jsx
+++ src/Components/ProductEdit/ProductEdit.jsx
@@ -5,7 +5,16 @@ function ProductEdit(){
 
     return(
         <>
+        <form>
+            <label htmlFor="">Name:</label>
+            <input type="text" />
 
+            <label htmlFor="">Price:</label>
+            <input type="text" />
+
+            <button>Cancel</button>
+            <button>Save Changes</button>
+        </form>
         </>
     );
 };
