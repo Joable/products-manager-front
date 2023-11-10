669b30e56168ed68656525bdb1a0030b4ccb1cc7
Joaquin <jaltable@fi.uba.ar>
2023-11-10 20:51:50 -0300

    created 'CreateProduct' component

---
+++ src/CreateProduct/CreateProduct.jsx
@@ -0,0 +1,11 @@
+import styles from './CreateProduct.module.css';
+
+function CreateProduct(){
+    return(
+        <>
+        CreatePRoduct
+        </>
+    );
+};
+
+export default CreateProduct;
